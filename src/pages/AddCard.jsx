28d01d7108c1725c5360269165be4bf9c5ea1bc0
Pedro Antonio <ped.santos@outlook.com.br>
2026-05-05 15:14:44 -0300

    teste

--- src/pages/AddCard.jsx
+++ src/pages/AddCard.jsx
@@ -182,7 +182,7 @@ const AddCard = () => {
       
       setNotification({
         type: 'success',
-        message: 'Cartão cadastrado com sucesso!'
+        message: 'Cartão cadastrado com sucesso!!'
       });
       
       setCardData({
