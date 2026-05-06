fa6b7431aa028c60d07dadd72a32b730822d0804
Pedro Antonio <ped.santos@outlook.com.br>
2026-05-06 17:39:19 -0300

    Codigo Refatorado

--- cypress/component/AddCard.cy.jsx
+++ cypress/component/AddCard.cy.jsx
@@ -1,19 +1,6 @@
 import React from 'react'
 import AddCard from '../../src/pages/AddCard'
 
-Cypress.Commands.add('alertErrorHaveText', (selector, expectedText) => {
-  cy.contains(selector, expectedText)
-    .should('be.visible')
-})
-
-Cypress.Commands.add('fillCardForm', (card) => {
-  cy.contains('label', 'Número do Cartão').parent().find('input').type(card.number)
-  cy.contains('label', 'Nome do Titular').parent().find('input').type(card.holderName)
-  cy.contains('label', 'Validade').parent().find('input').type(card.expirationDate)
-  cy.contains('label', 'CVV').parent().find('input').type(card.cvv)
-  cy.contains('button', card.bankLabel).click()
-})
-
 describe('<AddCard />', () => {
 
   beforeEach(() => {
@@ -38,112 +25,81 @@ describe('<AddCard />', () => {
     })
   })
 
-  it('Valida Cadastrar um cartão novo', () => {
-    const myCard = {
-      number: '5116 6096 7668 2899',
-      holderName: 'Pedro Teste',
-      expirationDate: '12/30',
-      cvv: '123',
-      bankLabel: 'Nubank'
-    }
+  it('Valida cadastrar um cartão novo', () => {
 
-    cy.intercept('POST', 'http://wallet.cardfify.dev/api/cards', (req) => {
-      req.reply({
+    cy.fixture('cards').then((data) => {
+
+      cy.intercept('POST', 'http://wallet.cardfify.dev/api/cards', {
         statusCode: 201,
-        body: { ...myCard, id: 1 }
-      })
-    }).as('addCard')
+        body: { ...data.validCard, id: 1 }
+      }).as('addCard')
 
-cy.fillCardForm(myCard)
+      cy.fillCardForm(data.validCard)
 
-    cy.contains('button', 'Adicionar Cartão').click()
-    cy.wait('@addCard')
+      cy.contains('button', 'Adicionar Cartão').click()
 
-    cy.contains('Cartão cadastrado com sucesso!').should('be.visible')
+      cy.wait('@addCard')
+
+      cy.contains('Cartão cadastrado com sucesso!')
+        .should('be.visible')
+    })
   })
 
   it('Valida nome com menos de 2 caracteres', () => {
-    const myCard = {
-      number: '5116 6096 7668 2899',
-      holderName: 'P',
-      expirationDate: '12/30',
-      cvv: '123',
-      bankLabel: 'Nubank'
-    }
 
-cy.fillCardForm(myCard)
+    cy.fixture('cards').then((data) => {
 
-    cy.contains('button', 'Adicionar Cartão').click()
+      cy.fillCardForm(data.invalidName)
 
-    cy.contains('Nome deve ter pelo menos 2 caracteres').should('be.visible')
+      cy.contains('button', 'Adicionar Cartão').click()
+
+      cy.contains('Nome deve ter pelo menos 2 caracteres')
+        .should('be.visible')
+    })
   })
 
-  it('ValidaData de expiração inválida1', () => {
-    const myCard = {
-      number: '5116 6096 7668 2899',
-      holderName: 'Pedro Teste 1',
-      expirationDate: '13/30',
-      cvv: '123',
-      bankLabel: 'Nubank'
-    }
+  it('Valida data de expiração inválida', () => {
 
-cy.fillCardForm(myCard)
+    cy.fixture('cards').then((data) => {
 
-    cy.contains('button', 'Adicionar Cartão').click()
+      cy.fillCardForm(data.invalidExpirationDate)
 
-    cy.contains('Data de expiração inválida ou vencida').should('be.visible')
+      cy.contains('button', 'Adicionar Cartão').click()
+
+      cy.contains('Data de expiração inválida ou vencida')
+        .should('be.visible')
+    })
   })
 
-  it('Valida Data de expiração inválida2', () => {
-    const myCard = {
-      number: '5116 6096 7668 2899',
-      holderName: 'Pedro Teste 2',
-      expirationDate: '12/00',
-      cvv: '123',
-      bankLabel: 'Nubank'
-    }
+  it('Valida CVV inválido', () => {
 
-cy.fillCardForm(myCard)
+    cy.fixture('cards').then((data) => {
 
-    cy.contains('button', 'Adicionar Cartão').click()
+      cy.fillCardForm(data.invalidCVV)
 
-    cy.contains('Data de expiração inválida ou vencida').should('be.visible')
+      cy.contains('button', 'Adicionar Cartão').click()
+
+      cy.contains('CVV deve ter 3 ou 4 dígitos')
+        .should('be.visible')
+    })
   })
 
-  it('ValidaCVV inválido', () => {
-    const myCard = {
-      number: '5116 6096 7668 2899',
-      holderName: 'Pedro Teste',
-      expirationDate: '01/30',
-      cvv: '12',
-      bankLabel: 'Nubank'
-    }
+  it('Valida não selecionar nenhum banco', () => {
 
-    cy.fillCardForm(myCard)
+    cy.fixture('cards').then((data) => {
 
-    cy.contains('button', 'Adicionar Cartão').click()
+      const cardWithoutBank = {
+        ...data.validCard,
+        bankLabel: null
+      }
 
-    cy.contains('CVV deve ter 3 ou 4 dígitos').should('be.visible')
-  })
+      cy.fillCardForm(cardWithoutBank)
 
-    it('Valida Não selecionar nenhum banco', () => {
-    const myCard = {
-      number: '5116 6096 7668 2899',
-      holderName: 'Pedro Teste',
-      expirationDate: '01/30',
-      cvv: '123',
-      bankLabel: 'Nubank'
-    }
+      cy.contains('button', 'Adicionar Cartão').click()
 
-    cy.contains('label', 'Número do Cartão').parent().find('input').type(myCard.number)
-    cy.contains('label', 'Nome do Titular').parent().find('input').type(myCard.holderName)
-    cy.contains('label', 'Validade').parent().find('input').type(myCard.expirationDate)
-    cy.contains('label', 'CVV').parent().find('input').type(myCard.cvv)
-    //cy.contains('button', myCard.bankLabel).click()
-
-    cy.contains('button', 'Adicionar Cartão').click()
-
-    cy.contains('Selecione um banco').should('be.visible')
+      cy.contains('Selecione um banco')
+        .should('be.visible')
+    })
   })
 
 })
